bf6d539b14e78dd954346106fda5dcf96c3cd10c
Tianyi Shen <tianyi.shen@cz-tek.com>
2014-03-18 18:32:53 +0800

    final version for icon

---
+++ EPM/EpmSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  EpmSettingViewController.h
+//  ClearInsight
+//
+//  Created by tianyi on 14-3-18.
+//  Copyright (c) 2014年 Cao Zhuo Information&Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EpmSettingViewController : UIViewController
+
+@end
